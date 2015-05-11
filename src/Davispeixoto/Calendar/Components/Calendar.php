05fb5038469f52857875ffb24758cc73d060ca9e
Davis L P Peixoto <davis.peixoto@gmail.com>
2015-05-11 19:16:30 -0300

    adding stuff

--- src/Davispeixoto/Calendar/Components/Calendar.php
+++ src/Davispeixoto/Calendar/Components/Calendar.php
@@ -13,5 +13,71 @@
  */
 class Calendar
 {
+    public $dtstamp;
+    public $uid;
+    public $dtstart;
+    public $dtend;
+    public $durantion;
+    public $class;
+    public $rrule;
 
+    public $attach;
+    public $attendee;
+    public $categories;
+    public $comment;
+    public $contact;
+    public $exdate;
+    public $rstatus;
+    public $related;
+    public $resources;
+    public $rdate;
+    public $x_prop;
+    public $iana_prop;
+
+
+    /*
+}
+*(
+;
+; The following are REQUIRED,
+; but MUST NOT occur more than once.
+;
+dtstamp / uid /
+;
+; The following is REQUIRED if the component
+; appears in an iCalendar object that doesn't
+                  ; specify the "METHOD" property; otherwise, it
+                  ; is OPTIONAL; in any case, it MUST NOT occur
+                  ; more than once.
+                  ;
+                  dtstart /
+                  ;
+                  ; The following are OPTIONAL,
+                  ; but MUST NOT occur more than once.
+                  ;
+                  class / created / description / geo /
+                  last-mod / location / organizer / priority /
+                  seq / status / summary / transp /
+                  url / recurid /
+                  ;
+                  ; The following is OPTIONAL,
+                  ; but SHOULD NOT occur more than once.
+                  ;
+                  rrule /
+                  ;
+                  ; Either 'dtend' or 'duration' MAY appear in
+                  ; a 'eventprop', but 'dtend' and 'duration'
+                  ; MUST NOT occur in the same 'eventprop'.
+                  ;
+                  dtend / duration /
+                  ;
+                  ; The following are OPTIONAL,
+                  ; and MAY occur more than once.
+                  ;
+                  attach / attendee / categories / comment /
+                  contact / exdate / rstatus / related /
+                  resources / rdate / x-prop / iana-prop
+                  ;
+                  )
+    */
 }
